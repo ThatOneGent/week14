f9b1db0f4548f4584abb89fe5d73a253ab5ae9b6
Markus <119375162+ThatOneGent@users.noreply.github.com>
2023-03-25 15:03:20 -0400

    clean up review form;adding usecase options

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -29,64 +29,56 @@ export default class ReviewForm extends React.Component {
             uindex: props.uindex
         }
         this.submitReview = this.submitReview.bind(this);
-        
+
     }
 
     submitReview() {
 
-   
-    
 
-       this.setState(state => ({ count: state.count + 1 }))
+
+
+        this.setState(state => ({ count: state.count + 1 }))
 
         const iReview = document.getElementById(`reviewText-${this.state.uindex}`).value;
         const iStar = parseInt(document.getElementById(`userStar-${this.state.uindex}`).value);
 
         console.log(iReview);
-        console.log(iStar);        
+        console.log(iStar);
 
         let newRevArray = this.state.reviews;
-        newRevArray.push(iReview);
-
         let newStarArray = this.state.srating;
-        newStarArray.push(iStar);
+
+        if(iReview == '' && iStar == 0){
+            window.alert('You must Submit a Review or Star Rating!');
+        }
+        if(iReview =='' && iStar != 0){
+            newStarArray.push(iStar);
+        }
+        if(iReview != '' && iStar ==0){
+            newRevArray.push(iReview);
+        }
+        if(iReview != '' && iStar != 0){
+            newRevArray.push(iReview);
+            newStarArray.push(iStar);
+        }
+
+      
 
 
         console.log(newRevArray);
-        //console.log("review state     " + this.state.reviews);
+        
         console.log(newStarArray)
 
 
-
-       // this.setState({reviews: newArray});
-        //this.setState({ reviews: newRevArray });
-        
         this.state.setNewReview(newRevArray);
-        //this.state.setNewStar(newStarArray);
-        
-       
-
-/*         <button onClick={() => {
-            setArtists([
-              ...artists,
-              { id: nextId++, name: name }
-            ]);
-          }}>Add</button> */
-       
-        //this.props.review.push(iReview);
-       // this.props.review.push(iStar);
-        //when button is clicked
-        // if text input, push to review array
-        // if star selection, push to srating array
-        // if both are empty, alert
 
 
     }
-    
+
 
     render() {
 
-      
+
         return (
             <div className="container-fluid">
                 <div className="">
@@ -98,18 +90,18 @@ export default class ReviewForm extends React.Component {
                             </div>
                             <div className="row">
                                 <div className="col-sm-3 gx-0">
-                                    <select className="form-select" id={`userStar-${this.state.uindex}`}> 
-                                        <option>Rating 1-10</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                        <option>7</option>
-                                        <option>8</option>
-                                        <option>9</option>
-                                        <option>10</option>
+                                    <select className="form-select" id={`userStar-${this.state.uindex}`}>
+                                        <option value='0'>Rating 1-10</option>
+                                        <option value='2'>1</option>
+                                        <option value='2'>2</option>
+                                        <option value='3'>3</option>
+                                        <option value='4'>4</option>
+                                        <option value='5'>5</option>
+                                        <option value='6'>6</option>
+                                        <option value='7'>7</option>
+                                        <option value='8'>8</option>
+                                        <option value='9'>9</option>
+                                        <option value='10'>10</option>
 
                                     </select>
 
